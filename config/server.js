89e430ab55474aebe976b5555c00c3f3865f9da6
Gu1ll4um3 <guillaumebongrand.lamanu@gmail.com>
2021-10-26 09:22:48 +0200

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c2e011ec6246525d794d9936cb4c5169'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '444b834c-d00e-4c2b-9986-e097e81da87c'
+};
